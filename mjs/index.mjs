54766292e31076e5fa40e397a4ff53766854718f
ariana0505 <penaariana075@gmail.com>
2025-02-14 20:32:56 -0500

    avanzando con .mjs

---
+++ mjs/index.mjs
@@ -0,0 +1,5 @@
+import { sum , rest , mult} from "./operadores.mjs";
+
+console.log(sum(2,3));
+console.log(rest(2,1));
+console.log(mult(3,5));
---
+++ mjs/operadores.mjs
@@ -0,0 +1,4 @@
+function sum (a,b) {return a + b};
+function rest (a,b) {return a - b};
+function mult (a,b) {return a * b};
+export{sum,rest,mult}
