311846ef0bbe081deb1e4fbf5fce64bb500ee5c6
ariana0505 <penaariana075@gmail.com>
2025-02-14 20:19:10 -0500

    avanzando cjs

--- cjs/index.js
+++ cjs/index.js
@@ -1,4 +1,3 @@
 //Puede cambiar nombre de la funcion extraido
-
 const sumar = require('./operador')
 console.log(sumar(3,4));
--- cjs/operador.js
+++ cjs/operador.js
@@ -1,3 +1,2 @@
 function sum (a,b) {return a + b}
-
 module.exports = sum
