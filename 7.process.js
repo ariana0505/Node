40ef3e0573d3f5fe91a2a090f8f842dab01537e9
ariana0505 <penaariana075@gmail.com>
2025-02-16 00:31:24 -0500

    process

---
+++ 7.process.js
@@ -0,0 +1,16 @@
+// argumentos de entrada
+//console.log(process.argv)
+
+// controlar el proceso y su salida 0 es que todo esta bien y 1 hubo error
+// process.exit(1)
+
+// podemos controlar eventos del proceso
+// process.on('exit', () => {
+//   // limpiar los recursos
+// })
+
+// current working directory
+console.log(process.cwd()) //desde que carpeta se a ejecutado del directorio
+
+// platform
+console.log(process.env.PEPITO)
